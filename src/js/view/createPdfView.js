433b0c46d1f84651ef69cf78fd7ef1ffab6f2c17
yaameen <yaamynu@gmail.com>
2021-09-07 05:20:25 +0500

    Update createPdfView.js

--- src/js/view/createPdfView.js
+++ src/js/view/createPdfView.js
@@ -47,7 +47,7 @@ export const createPdfView = (_) =>
             if (isPreviewablePdf(item.file)) {
               root.dispatch("DID_UPDATE_PANEL_HEIGHT", {
                 id: id,
-                height: root.ref.pdf.scrollHeight,
+                height: numPdfPreviewHeight,
               });
             }
           },
